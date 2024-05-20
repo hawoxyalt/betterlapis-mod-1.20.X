ae5ed537a47014f10545c8519be85edf5275b9fa
hawoxy <hawoxy@gmail.com>
2024-05-19 21:35:19 -0400

    Initial commit

---
+++ src/main/java/net/hawoxalt/betterlapis/BetterLapis.java
@@ -0,0 +1,18 @@
+package net.hawoxalt.betterlapis;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterLapis implements ModInitializer {
+	public static final String MOD_ID = "betterlapis";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/hawoxalt/betterlapis/BetterLapisClient.java
@@ -0,0 +1,10 @@
+package net.hawoxalt.betterlapis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterLapisClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
